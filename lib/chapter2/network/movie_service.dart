843c211ba50162519a97e45c62b92318b205ce5a
Csongor Vogel <gerfalcon.vogel@gmail.com>
2023-04-02 03:18:47 +0400

    Do the network call by using http

---
+++ lib/chapter2/network/movie_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert' as convert;
+
+import 'package:flutter_intro_movie_app/chapter2/model/movie.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+/// Register to imdb API (https://imdb-api.com/) and get an own apiKey for FREE.
+const String apiKey = 'todo';
+
+Future<List<Movie>> fetchPopularMovies() async {
+  final Response response = await http.get(
+    Uri.parse('https://imdb-api.com/en/API/MostPopularMovies/$apiKey'),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<Movie> movies = [];
+    int numberOfMovie = 100;
+    var data = convert.jsonDecode(response.body);
+    for (int i = 0; i < numberOfMovie; i++) {
+      movies.add(Movie.fromJson(data["items"][i]));
+    }
+    return movies;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception(
+      'Failed to load movies, error code: ${response.statusCode}',
+    );
+  }
+}
